209adf1d9d222e08eb0a9f038e304687202cb6e0
Kapási Fülöp <fulopkapasi@gmail.com>
2025-06-29 23:40:33 +0200

    phpstan + helper fix

--- src/Helper.php
+++ src/Helper.php
@@ -1,5 +1,7 @@
 <?php
 
+use LarasoftHU\LocalizedRoutesPlus\LocalizedRoute;
+
 if (! function_exists('route_is')) {
     /**
      * Check if the current route name matches the given pattern.
@@ -8,6 +10,9 @@ if (! function_exists('route_is')) {
      */
     function route_is(string $name): bool
     {
-        return request()->route()->is($name);
+        /** @var LocalizedRoute|null $route */
+        $route = request()->route();
+
+        return $route !== null && $route->is($name);
     }
 }
