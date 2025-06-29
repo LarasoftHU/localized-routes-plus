5cd5a09b29f320799dac2b8befa96da473dfb6b7
Kapási Fülöp <fulopkapasi@gmail.com>
2025-06-29 16:35:12 +0200

    test

--- src/LocalizedRouter.php
+++ src/LocalizedRouter.php
@@ -21,30 +21,4 @@ class LocalizedRouter extends Router
         parent::__construct($events, $container);
     }
 
-    public function newRoute($methods, $uri, $action)
-    {
-        return (new Route($methods, $uri, $action))
-            ->setRouter($this)
-            ->setContainer($this->container);
-    }
-
-    /**
-     * Route a resource to a controller.
-     *
-     * @param  string  $name
-     * @param  string  $controller
-     * @return \LarasoftHU\LocalizedRoutesPlus\PendingLocalizedRouteRegistration
-     */
-    public function resource($name, $controller, array $options = [])
-    {
-        if ($this->container && $this->container->bound(LocalizedResourceRegistrar::class)) {
-            $registrar = $this->container->make(LocalizedResourceRegistrar::class);
-        } else {
-            $registrar = new LocalizedResourceRegistrar($this);
-        }
-
-        return new PendingLocalizedRouteRegistration(
-            $registrar, $name, $controller, $options
-        );
-    }
 }
